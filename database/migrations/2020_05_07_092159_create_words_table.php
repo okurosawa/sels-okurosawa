9b5e16da3cf3853d05a3d1d7ae3efed2d629623a
okurosawa <okurosawa@bizmates.jp>
2020-05-07 18:23:41 +0900

    Create word migration and model

---
+++ database/migrations/2020_05_07_092159_create_words_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Symfony\Component\Console\Helper\Table;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('content');
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign key
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
