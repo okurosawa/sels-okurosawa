9884f3e7ee76ae3f5c882d85d0bc570d59ebcc7a
Osamu Kurosawa <56111888+okurosawa@users.noreply.github.com>
2020-05-08 16:16:05 +0900

    Feature/create_top_login_register_page (#2)
    * Authentication scaffolding generated
    
    Use 'first_name' and 'last_name' instead of 'name'
    
    create directory related to users view
    
    create header
    
    * fix indent of css 4 spaces to 2 spaces

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,58 @@
+<div class="top-gradation-border">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel shadow">
+        <div class="container">
+            @guest
+                <a class="navbar-brand font-weight-bold" href="{{ url('/') }}">
+                    {{ config('app.name', 'E-Learning') }}
+                </a>
+            @else
+                <a class="navbar-brand font-weight-bold" href="{{ url('/home') }}">
+                    {{ config('app.name', 'E-Learning') }}
+                </a>
+            @endguest
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+    
+                </ul>
+    
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->first_name }} <span class="caret"></span>
+                            </a>
+    
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+    
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
